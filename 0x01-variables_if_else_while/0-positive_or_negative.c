42d120ef03e54c407a781b110ea4151896c90377
John Agbo <Jonchukwuma04@gmail.com>
2022-04-08 07:46:26 -0700

    This commit demonstartes the use of conditionals

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("%d if the number is greater than 0: is positive\n", 'n');
+	}
+	else if n = 0;
+	{
+	printf("%d if the number is is 0: is zero\n", 'n');
+	}
+	else if (n < 0)
+	{
+	printf("%d if the number is less than 0: is negative\n", 'n');
+	}
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This script evaluates to demonstrate conditionals
