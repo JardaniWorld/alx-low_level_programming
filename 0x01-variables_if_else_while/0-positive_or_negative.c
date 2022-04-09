b9a8972e5d344498c7c8c8213181889834df3bc7
John Agbo <Jonchukwuma04@gmail.com>
2022-04-09 08:44:02 -0700

    This commit checks the sign of a number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,29 @@
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <time.h>
 /**
  * main - Entry point
- * 
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-	{
-	printf("%d if the number is greater than 0: is positive\n", 'n');
-	}
-	else if n = 0;
-	{
-	printf("%d if the number is is 0: is zero\n", 'n');
-	}
-	else if (n < 0)
-	{
-	printf("%d if the number is less than 0: is negative\n", 'n');
-	}
-	return (0);
-
+if (n > 0)
+{
+printf("%d if the number is greater than 0: is positive\n", n);
+}
+else if
+(n = 0);
+{
+printf("%d if the number is 0: is zero\n", n);
+}
+if (n < 0)
+{
+printf("%d if the number is less than 0: is negative\n", n);
+}
+return (0);
 }
