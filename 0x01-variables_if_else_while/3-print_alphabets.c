f7a9d6d68ee3db012c4c25e884f2d406dc3cf034
John Agbo <Jonchukwuma04@gmail.com>
2022-04-11 05:54:43 -0700

    This commit prints both lower and upper cases of the letters of the alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char ch;
-char ch1;
+char ch = 'a';
+char ch1 = 'A';
 
 for (ch = 'a'; ch <= 'z'; ch++)
 {
@@ -17,7 +17,7 @@ putchar(ch);
 for (ch1 = 'A'; ch1 <= 'Z'; ch1++)
 {
 putchar(ch1);
-putchar('\n');
 }
+putchar('\n')
 return (0);
 }
