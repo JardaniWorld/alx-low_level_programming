05f9fa9c9ae87ddde7098a8ac32ed7f9cb49f369
John Agbo <Jonchukwuma04@gmail.com>
2022-04-10 18:14:39 -0700

    This commit prints out a string to the display

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}
+This script executes to print a string to the display
