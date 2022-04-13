ce6a998652389b075dc85a98aff7fa4edd930b1e
John Agbo <Jonchukwuma04@gmail.com>
2022-04-13 13:08:14 -0700

    This commit prints out n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,6 +36,6 @@ _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(k / 10 % 10 + '0');
-_putchar(k % 10 + '0'); } }
-_putchar('\n'); } }
+_putchar(k % 10 + '0'); } } }
+_putchar('\n'); }
 
