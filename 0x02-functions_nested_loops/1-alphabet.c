1ee28c37a4d53f41f99f091cd1a105486261c60e
John Agbo <Jonchukwuma04@gmail.com>
2022-04-13 00:19:48 -0700

    This commit displays letters of the alphabet using the write function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -Entry point
+ * print_alphabet - void function
+ * Return: Always 0. 
+ */
+
+int main(void)
+{
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+_putchar('\n');
+}
+
+}
+}
