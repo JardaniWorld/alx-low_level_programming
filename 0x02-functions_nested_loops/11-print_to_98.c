03257abc5e077e5d3785be457d20b1ee939b637b
John Agbo <Jonchukwuma04@gmail.com>
2022-04-13 09:02:56 -0700

    This commit prints all natural numbers to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,39 @@
 void print_to_98(int n)
 {
 
-if (n < 98)
+int i;
+
+if (n > 98)
 {
-for (n = n; n < 98; n++)
+
+for (i = n; i > 97; i--)
 {
-printf("%d, ", n);
-printf("%d\n", 98);
+printf("%d", i);
+
+if (i != 98)
+{
+printf(", ");
 }
 }
 
+}
+
 else
+
 {
-for (n = n; n > 98; n--)
+
+for (i = n; i < 99; i++)
 {
-printf("%d, ", n);
-printf("%d\n, ", 98);
+printf("%d", i);
+
+if (i != 98)
+{
+printf(", ");
 }
+
 }
+
+}
+printf("\n");
 
 }
