2deb1c61f4c2a49b13a5b834b143869bb95b88b5
John Agbo <Jonchukwuma04@gmail.com>
2022-04-14 11:57:50 -0700

    This commit prints the remaining days of the year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,21 +12,10 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-
+if (year % 4 == 0 || ((year % 400 == 0) && (year % 100 == 0)))
 {
-if (month > 2)
-{
-day++;
-}
 
-printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
-}
-
-if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-{
-if (month > 2)
+if (month > 2 && day >= 60)
 {
 day++;
 }
@@ -36,11 +25,19 @@ printf("Remaining days: %d\n", 366 - day);
 
 else
 {
+if (month == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+
+else
+{
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
 
+}
 
 
 
