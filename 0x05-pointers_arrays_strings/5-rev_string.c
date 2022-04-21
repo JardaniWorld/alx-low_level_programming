be82ac0ac1fe4ae854774773f12dc09b9b061651
John Agbo <Jonchukwuma04@gmail.com>
2022-04-21 13:08:31 -0700

    This commit reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,7 +9,7 @@
 
 void rev_string(char *s)
 {
-char *star_c, *end_c, c;
+char *start_c, *end_c, c;
 int i, count;
 int length = 0;
 
@@ -18,8 +18,10 @@ for (i = 0; s[i]; i++)
 length++;
 }
 count = length;
+
 start_c = s;
 end_c = s;
+
 
 for (i = 0; i < count - 1; i++)
 {
@@ -30,9 +32,9 @@ for (i = 0; i < count / 2; i++)
 {
 c = *end_c;
 *end_c = *start_c;
-*start_c = *start_c;
-
+*start_c = c;
 start_c++;
 end_c--;
 }
+
 }
