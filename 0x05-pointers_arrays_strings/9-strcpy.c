059ae461eefc835f7cc6f0985c8afe9c4e23ef08
John Agbo <Jonchukwuma04@gmail.com>
2022-04-21 17:28:02 -0700

    This commit copies the string pointed to by a pointer into a buffer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
-int i;
+int count = 0;
 
-while (src[i] != '\0')
+while (count >= 0)
 {
-dest[i] = src[i];
-i++;
+*(dest + count) = *(src + count);
+
+if (*(src + count) == '\0')
+break;
+count++;
 }
-dest[i] = '\0';
 
 return (dest);
 }
