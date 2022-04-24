add6f44e3f9df4f5b02cdcfa15e0983321bd6dc7
John Agbo <Jonchukwuma04@gmail.com>
2022-04-24 10:22:51 -0700

    This commit concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strcat - This function concatenates two string
+ *
+ * @dest: one of the strings to be concatenated
+ * @src: The second string to be concatenated
+ *
+ * Return: Always 0. (Success)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+char *tmp;
+char concStr;
+tmp = &concStr;
+concStr = strcpy(*dest, *src);
+
+char str1[10] = "dest";
+char str2[10] = "src";
+char conc;
+char *a, *b, *c;
+
+a = &str1;
+b = &str2;
+
+while (*a != '\0')
+{
+a++;
+}
+
+while (*b != '\0')
+{
+c = strcpy(a, b);
+b++;
+}
+
+conc = dest;
+
+return (conc);
+
+}
