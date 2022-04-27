babbff5f1696a65a9beb1beda1e6942055a9f27a
John Agbo <Jonchukwuma04@gmail.com>
2022-04-26 19:16:47 -0700

    This commit locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strchr - This function locates a character in a string
+ * @s: The string
+ * @c: The character to be located
+ *
+ * Return: A pointer to the first occurrence of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+char i;
+i = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == 'c')
+{
+return (*c = 'c');
+}
+
+else
+{
+return (NULL);
+}
+
+i++;
+}
+
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-This C code executes to coopye one memory into another
+This C code executes to locate a character in a string
