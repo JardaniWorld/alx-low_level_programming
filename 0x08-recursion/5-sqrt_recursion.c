df5886bfd79f6fe4070b9faf2262c6e74bbe999d
John Agbo <Jonchukwuma04@gmail.com>
2022-04-28 18:11:44 -0700

    This commit calculates the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,29 +1,54 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - This function returns the natural
- * square root of a number
+ * _analysis- This function analyzes the base case conditions
+ * for the square root function
  *
- * @n: Square root of number to be computed
+ * @i: an integer
+ * @n: an integer
+ *
+ * Return: _analysis
+ */
+
+int _analysis(int i, int n)
+{
+if (n == 0 || n == 1)
+{
+return (n);
+}
+else if (i * i < n)
+{
+return (_analysis(i + 1, n));
+return (-1);
+}
+else if (i * i == n)
+{
+return (i);
+}
+
+return (-1);
+}
+
+/**
+ * _sqrt_recursion - This function calculates the natural square root
+ * of a number
+ *
+ * @n: The number to be calculated
  *
  * Return: Always 0. (Success)
  */
 
 int _sqrt_recursion(int n)
 {
-int x;
-
-if (!(n * n))
+int i = 0;
+if (i < 0)
 {
 return (-1);
 }
 
-if (x < (n / 2))
+else
 {
-if ((n % x) == 0)
-{
-return (_sqrt_recursion(x * x));
-}
+return (_analysis(i, n));
 }
 
 return (0);
