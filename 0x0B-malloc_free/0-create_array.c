c049f318c585c088c9ef27871ede682ff181e94f
John Agbo <Jonchukwuma04@gmail.com>
2022-05-05 17:45:28 -0700

    This commit creates an array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - This function creates an array of characters
+ * @size: bytes to be allocated
+ * @c: An array of characters
+ *
+ * Return: Always 0. (Success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char c[] = "John";
+c[0] = 'J';
+
+if (size == 0)
+{
+return (NULL);
+}
+
+else
+{
+return (create_array);
+}
+
+return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0. (Success)
+ */
+
+int main(void)
+{
+create_array();
+
+return (0);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This code creates and array of characters and initializes it with a specific character
