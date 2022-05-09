b685df8a93af86eba615735de4bbfe4227e9a454
John Agbo <Jonchukwuma04@gmail.com>
2022-05-09 14:09:37 -0700

    This commit allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - This function allocates memory using malloc
+ * @b: A variable
+ *
+ * Return: A pointer to the allocated space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+
+ptr = malloc(sizeof(unsigned int) * b);
+
+if (ptr == NULL)
+{
+exit(98);
+}
+
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This code allocates memory using malloc
