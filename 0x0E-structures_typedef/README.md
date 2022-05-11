0713fef245e0db5f18a6bace9ad6b2d6f25853c6
John Agbo <Jonchukwuma04@gmail.com>
2022-05-11 16:59:26 -0700

    This commit creates a new type with some elements

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This code creates a new type with some elements
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - This defines anew type with some properties
+ * @name: A parameter
+ * @age: A parameter
+ * @owner: A parameter
+ *
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
