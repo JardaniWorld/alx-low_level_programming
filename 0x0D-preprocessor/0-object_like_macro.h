e426883158b9da10e2455e2b052e04c4a28d700b
John Agbo <Jonchukwuma04@gmail.com>
2022-05-11 12:50:38 -0700

    This commit creates a macro with token

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,6 +3,17 @@
 #include "0-object_like_macro.h"
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0. (Success)
+ */
+
+int main(void)
+{
 #define SIZE 1024
+
+return (0);
+}
 
 #endif
