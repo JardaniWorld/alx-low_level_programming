924db3600735415b541b46369f9cf5fb1e4e6733
John Agbo <Jonchukwuma04@gmail.com>
2022-05-13 15:04:01 -0700

    This commit prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - This function prints a name
+ * @name: A variable of type pointer to char
+ * @f: A pointer to a function
+ *
+ * Return: nothing to return
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+char *name1;
+*name = &name1;
+
+_putchar(name);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0. (Success)
+ */
+
+int main(void)
+{
+void (*f)(char *) = print_name;
+print_name();
+
+return (0);
+}
